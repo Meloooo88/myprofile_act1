a37f4ee35cc9008e0d44268019c161e7f1340b3b
Meloooo88 <brosotoadiel48@gmail.com>
2025-04-30 11:38:38 +0800

    edit

--- myprofile/src/components/Home.js
+++ myprofile/src/components/Home.js
@@ -1,7 +1,7 @@
 const Home = () => (
     <div>
       <h1>Welcome!</h1>
-      <p>This is my personal website. I'm 3rd year IT students from University of Cabuyao</p>
+      <p>This is my personal website. I'm Adiel Mhelo Brosoto 3rd year IT students from University of Cabuyao</p>
     </div>
   );
   
