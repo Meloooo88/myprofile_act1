cce3d13cd428a2a14b5ea9d47208da778185d03f
Meloooo88 <brosotoadiel48@gmail.com>
2025-04-30 12:14:04 +0800

    lasr

--- myprofile/src/components/Footer.js
+++ myprofile/src/components/Footer.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
-const Footer = () => (
-  <footer>
-    <p>&copy; 2025 Adiel Mhelo Brosoto</p>
-  </footer>
-);
+const footerContainer = {
+  display: 'flex',
+  justifyContent: 'center',
+  marginTop: 'auto',
+  width: '100%',
+};
+
+const footerStyle = {
+  padding: '1rem',
+  textAlign: 'center',
+  backgroundColor: '#000000',
+  color: '#ffffff',
+  width: '100%',
+  maxWidth: '100%',
+  borderRadius: '8px',
+};
+
+const Footer = () => {
+  return (
+    <div style={footerContainer}>
+      <footer style={footerStyle}>
+        © {new Date().getFullYear()} Adiel Mhelo Brosoto. All rights reserved.
+      </footer>
+    </div>
+  );
+};
 
 export default Footer;
